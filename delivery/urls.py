40c7c11a56ad6695f668008c6ba42f4c5841dbf5
shruti-jaiswal08 <113632591+shruti-jaiswal08@users.noreply.github.com>
2022-09-23 13:14:17 +0530

    Delete delivery directory

--- delivery/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.delivery , name="Delivery"),
-    path('<int:id>', views.delivery_details , name="Delivery Details"),
-]
